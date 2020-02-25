4ed3c697f06360b355d74135ef78250c036e02b0
Noereddien <irshad.n.b@hotmail.com>
2020-02-25 16:59:23 +0100

    PickingUp scrap and make player bigger
    The player can now pickup scrap a becomes bigger after a certain limit.

---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/CollectScrap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectScrap : MonoBehaviour
+{
+
+    PlayerScrap scrap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scrap = FindObjectOfType<PlayerScrap>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Hull")
+        {
+            scrap.scrap += 1;
+
+            Destroy(gameObject);
+        }
+    }
+}
