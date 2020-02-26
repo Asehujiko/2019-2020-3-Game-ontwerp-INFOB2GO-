2d840eabbe215c2e53aa36928121e45bbe389d88
Jaykayha <jkh1337@hotmail.com>
2020-02-26 14:55:24 +0100

    Main menu
    Added main menu (buttons not functiona yet)

---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/MouseHover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour
+{
+    public Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.black;
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = Color.black;
+    }
+}
