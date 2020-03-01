6b81c06365320b2b621f75e7f0d206bbeb2c4017
Jaykayha <57985939+Jaykayha@users.noreply.github.com>
2020-03-01 16:30:21 +0100

    Updated main menu
    Start and exit buttons work now

---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // these are the buttons on the main menu
+    public bool isStart, isHighscore, isSettings, isExit;
+
+    void OnMouseUp()
+    {
+        if (isStart)
+            SceneManager.LoadScene("MyScene");
+        if (isHighscore)
+            return;
+        if (isSettings)
+            return;
+        if (isExit)
+            Application.Quit();
+    }
+}
