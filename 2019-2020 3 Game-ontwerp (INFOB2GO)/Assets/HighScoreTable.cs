2adebe1bf394a20aa8ab72ad30e249948c15ce4b
Jaykayha <57985939+Jaykayha@users.noreply.github.com>
2020-03-14 22:12:33 +0100

    New high scores list
    New high scores list, this one scales properly with different resolutions and aspect ratios

---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/HighScoreTable.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreTable : MonoBehaviour
+{
+
+    public Text highScoreNames;
+    public Text highScores;
+    private List<string> names = new List<string>();
+    private List<string> scores = new List<string>();
+
+    void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            names.Add(PlayerPrefs.GetString("High Score Name" + i, "Empty"));
+            scores.Add((PlayerPrefs.GetInt("High Score" + i, 0)).ToString());
+        }
+        
+        highScoreNames.text = ListToText(names);
+        highScores.text = ListToText(scores);
+    }
+
+    private string ListToText(List<string> list)
+    {
+        string text = "";
+        foreach (var i in list)
+        {
+            text += i.ToString() + "\n";
+        }
+
+        return text;
+    }
+}
