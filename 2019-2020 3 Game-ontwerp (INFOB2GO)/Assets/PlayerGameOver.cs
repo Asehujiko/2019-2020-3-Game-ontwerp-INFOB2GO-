8a9b09083626a97d24937cbaf55b31a70d86cda3
Jaykayha <57985939+Jaykayha@users.noreply.github.com>
2020-03-16 17:18:06 +0100

    Added main-menu stuff

---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/PlayerGameOver.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Delete))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        GameObject GameOverObject = GameObject.FindWithTag("GameOver");
+        GameOverObject.GetComponent<PlayerHighScore>().enabled = true;
+        Cursor.visible = true;
+    }
+}
---
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/Scripts/Menu/HighScoreTable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreTable : MonoBehaviour
+{
+
+    public Text highScoreNames;
+    public Text highScores;
+    public List<string> names = new List<string>();
+    public List<string> scores = new List<string>();
+
+    void Start()
+    {
+        DrawTable();
+    }
+
+    public void DrawTable()
+    {
+        // adds the high scores and the names associated with it to a scores and names list
+        for (int i = 0; i < 10; i++)
+        {
+            names.Add(PlayerPrefs.GetString("High Score Name" + i, "Empty"));
+            scores.Add((PlayerPrefs.GetInt("High Score" + i, 0)).ToString());
+        }
+
+        // the string of the high scores and names will be added to the high score table which will then be drawn on the screen
+        highScoreNames.text = ListToText(names);
+        highScores.text = ListToText(scores);
+    }
+
+    // the content of the aforementioned lists will be converted to a string
+    private string ListToText(List<string> list)
+    {
+        string text = "";
+        foreach (var i in list)
+        {
+            text += i.ToString() + "\n";
+        }
+
+        return text;
+    }
+}
