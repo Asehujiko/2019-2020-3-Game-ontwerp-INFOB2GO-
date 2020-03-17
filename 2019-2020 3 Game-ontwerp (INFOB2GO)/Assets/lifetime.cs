800e55dea6cf57116daf50f998570403763136dc
LRV2K1 <l.r.verhage@hetnet.nl>
2020-03-17 16:34:02 +0100

    map update

--- 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/lifetime.cs
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/lifetime.cs
@@ -8,7 +8,7 @@ public class lifetime : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this, life);
+        Destroy(gameObject, life);
     }
 
     // Update is called once per frame
