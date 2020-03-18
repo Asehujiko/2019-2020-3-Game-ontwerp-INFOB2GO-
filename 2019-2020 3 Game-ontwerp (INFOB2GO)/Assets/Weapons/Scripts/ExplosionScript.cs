481bbad0715ce19ca96505cfa179235ef2a690fc
LRV2K1 <l.r.verhage@hetnet.nl>
2020-03-18 13:54:19 +0100

    projectile particle effects

--- 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/Weapons/Scripts/ExplosionScript.cs
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/Weapons/Scripts/ExplosionScript.cs
@@ -8,22 +8,14 @@ public class ExplosionScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Destroy(gameObject, 2);
         AudioSource audio = GetComponent<AudioSource>();
         GetComponent<AudioSource>().PlayOneShot(clip);
-        GetComponent<MeshRenderer>().enabled = false;
-        StartCoroutine(WaitDestroy());
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        gameObject.transform.localScale += new Vector3(+0.2f, +0.2f, +0.2f);
-
     }
 
     IEnumerator WaitDestroy()
     {
         yield return new WaitForSeconds(1);
-        Destroy(gameObject, 0.05f);
+        Destroy(gameObject, 0.5f);
     }
 }
