00b53cbdb64905bf74c4060f3788308483606494
LRV2K1 <l.r.verhage@hetnet.nl>
2020-03-18 11:29:33 +0100

    HP bar

--- 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/HPIndicator.cs
+++ 2019-2020 3 Game-ontwerp (INFOB2GO)/Assets/HPIndicator.cs
@@ -6,16 +6,13 @@ public class HPIndicator : MonoBehaviour
 {
     public HealthController health;
     public GameObject healthbar;
-    Vector3 startposition;
-    Vector3 healthstartposition;
+    public GameObject overlay;
     int currenthealth;
     int maxhealth;
     public int maxlength = 175;
 
     private void Start()
     {
-        healthstartposition = healthbar.transform.localPosition;
-        startposition = transform.position;
         maxhealth = health.maxhealth;
         currenthealth = health.health;
     }
@@ -26,7 +23,7 @@ public class HPIndicator : MonoBehaviour
         maxhealth = health.maxhealth;
 
         float offset = (float)maxlength -(float)maxlength * ((float)currenthealth / (float)maxhealth);
-        transform.position = new Vector3(offset + startposition.x, transform.position.y, transform.position.z);
-        healthbar.transform.localPosition = healthstartposition + new Vector3(-offset, 0, 0);
+        overlay.transform.localPosition = new Vector3(offset, 0, 0);
+        healthbar.transform.localPosition = new Vector3(-offset, 0, 0);
     }
 }
